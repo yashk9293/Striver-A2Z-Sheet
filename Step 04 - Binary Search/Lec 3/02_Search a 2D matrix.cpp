3104da0235f8cef558b29173dfcf5f7fc4907a84
Yash Kumar <yashk9293@gmail.com>
2025-06-03 22:34:52 +0530

    changes

--- Step 04 - Binary Search/Lec 3/02_Search a 2D matrix.cpp
+++ Step 04 - Binary Search/Lec 3/02_Search a 2D matrix.cpp
@@ -1,24 +1,23 @@
 // Question Link :- https://leetcode.com/problems/search-a-2d-matrix/
 // Search a 2D matrix
 
-// Approach - 1 (same code as Search a 2D matrix II)
-// T.C = O(m+n)
+
+
+// Approach - 1 (Brute Force)
+// T.C = O(m*n)
 // S.C = O(1)
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int i = 0;
-        int j = n-1;
-        while(i < m && j >= 0) {
-            if(matrix[i][j] == target) {
-                return true;
-            }
-            if(matrix[i][j] < target) {
-                i++;
-            } else if(matrix[i][j] > target) {
-                j--;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        // Brute force: check every element
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
             }
         }
         return false;
@@ -28,9 +27,40 @@ public:
 
 
 
+// Approach - 2 (Binary Search)
+// T.C = O(m*logn)
+// S.C = O(1)
+class Solution {
+public:
+    bool binarySearchRow(vector<int>& row, int target) {
+        int left = 0, right = row.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (row[mid] == target) {
+                return true;
+            } else if (row[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for (auto& row : matrix) {
+            if (binarySearchRow(row, target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
 
 
-// Approach - 2
+
+
+// Approach - 3 (Binary Search on the entire matrix)
 // T.C = O(log(m*n))
 // S.C = O(1)
 class Solution {
@@ -52,6 +82,33 @@ public:
             }
             else {
                 high = mid - 1;
+            }
+        }
+        return false;
+    }
+};
+
+
+
+
+// Approach - 4 [Optimal  Solution] (same code as Search a 2D matrix II)
+// T.C = O(m+n)
+// S.C = O(1)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int i = 0;
+        int j = n-1;
+        while(i < m && j >= 0) {
+            if(matrix[i][j] == target) {
+                return true;
+            }
+            if(matrix[i][j] < target) {
+                i++;
+            } else if(matrix[i][j] > target) {
+                j--;
             }
         }
         return false;
--- Step 04 - Binary Search/Lec 3/03_Search a 2D Matrix II.cpp
+++ Step 04 - Binary Search/Lec 3/03_Search a 2D Matrix II.cpp
@@ -1,7 +1,63 @@
-// Question Link :- https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+// Question Link :- https://leetcode.com/problems/search-a-2d-matrix-ii/
 // Search a 2D Matrix II
 
 
+// Approach - 1 (Brute Force)
+// T.C = O(m*n)
+// S.C = O(1)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        // Brute force: check every element
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
+
+
+
+// Approach - 2 (Binary Search)
+// T.C = O(m*logn)
+// S.C = O(1)
+class Solution {
+public:
+    bool binarySearchRow(vector<int>& row, int target) {
+        int left = 0, right = row.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (row[mid] == target) {
+                return true;
+            } else if (row[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for (auto& row : matrix) {
+            if (binarySearchRow(row, target)) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+
+// Optimal Solution (Using Pointers)
 // T.C = O(logn)
 // S.C = O(1)
 class Solution {
